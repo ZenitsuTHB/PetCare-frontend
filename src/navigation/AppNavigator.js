357562917d60985aa67e99774d684c93d847acfa
Sergi <66616297+SergiMT@users.noreply.github.com>
2025-09-17 14:03:31 +0200

    Cambio en el Navigator NewPetScreen

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import NewPetFormScreen from '../screens/Forms/NewPetForm/NewPetFormScreen';
+import NewPetFormScreen from '../components/Forms/NewPetFormScreen';
 import LandingScreen from '../screens/Landing/LandingScreen';
 import LoginScreen from '../screens/Login/LoginScreen';
 import RegisterScreen from '../screens/Register/RegisterScreen';
