ea5e56a21fc62f370ae5131e71d93bcc570d306b
ZenitsuTHB <starxyz12@gmail.com>
2025-10-14 12:53:11 +0200

    tiny change in the uploaddocument

--- src/screens/Archivos/UploadDocumentScreen.js
+++ src/screens/Archivos/UploadDocumentScreen.js
@@ -121,7 +121,6 @@ export default function UploadDocumentScreen({ route, navigation }) {
           {formErrors.file ? <Text style={styles.errorText}>{formErrors.file}</Text> : null}
           {fileName ? <Text style={styles.fileName}>Archivo seleccionado: {fileName}</Text> : null}
         </View>
-     
       </View>
     </View>
   );
